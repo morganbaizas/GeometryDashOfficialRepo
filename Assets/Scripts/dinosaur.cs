ead2cf32450cc031d2f6ac11fee13edd0c2ac12e
morganbaizas <98178341+morganbaizas@users.noreply.github.com>
2023-11-27 14:46:59 -0500

    did something with the jumping :)

--- Assets/Scripts/dinosaur.cs
+++ Assets/Scripts/dinosaur.cs
@@ -6,7 +6,7 @@ public class dinosaur : MonoBehaviour
 {
     private Rigidbody2D playerRb;
     private float x = 3f;
-    public bool touchingGround = true;
+    public bool touchingGround = false;
 
 
     void Start()
@@ -21,6 +21,16 @@ public class dinosaur : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && touchingGround)
         {
             playerRb.AddForce(Vector3.up * x, ForceMode2D.Impulse);
+        }
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "platform1")
+        {
+            touchingGround = true;
         }
     }
 }
