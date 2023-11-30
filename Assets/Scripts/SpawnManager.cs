5e1bd6e53f585025dcf0dc63c26fdf373b780887
morganbaizas <98178341+morganbaizas@users.noreply.github.com>
2023-11-30 15:14:01 -0500

    we got many to spawn, but on the same y :)

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,16 +8,22 @@ public class SpawnManager : MonoBehaviour
     private static readonly Random rnd = new Random();
     public GameObject platformPrefab;
     private Vector2 spawnPosition = new Vector2(25, (rnd.Next(0, 5) -4));
+    private float startDelay = 2;
+    private float repeatRate = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(platformPrefab, spawnPosition, platformPrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle() {
+        Instantiate(platformPrefab, spawnPosition, platformPrefab.transform.rotation);
     }
 }
