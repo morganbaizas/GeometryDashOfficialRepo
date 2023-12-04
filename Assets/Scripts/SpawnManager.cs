031df73a8b94e2de7eed2f4eac50626699f0c7c9
Micah Leifer <98467381+miclei25@users.noreply.github.com>
2023-12-04 00:57:19 -0500

    spawn manager updates from thursday :)

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,17 +7,34 @@ public class SpawnManager : MonoBehaviour
 {
     private static readonly Random rnd = new Random();
     public GameObject platformPrefab;
-    private Vector2 spawnPosition = new Vector2(25, (rnd.Next(0, 5) -4));
+    private Vector2 spawnPosition;
+    private float startDelay = 0;
+    private float repeatRate = 2;
+
+    if (platformPrefab)
+    {
+        random = (rnd.Next(0, 5)) - 4;
+        spawnPosition = new Vector2(25, random);
+    } else
+    {
+        random = (rnd.Next(1, 5) - 4);
+        spawnPosition = new Vector2(25, random);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(platformPrefab, spawnPosition, platformPrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(platformPrefab, spawnPosition, platformPrefab.transform.rotation);
     }
 }
